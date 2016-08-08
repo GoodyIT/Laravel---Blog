78138f745c1a1d192d240257020a32a50e51a53f
Rexford <seanmavley@gmail.com>
2016-08-08 10:49:44 +0000

    initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        {!! MaterializeCSS::include_css() !!}
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+
+    <body>
+      <div class="container">
+          <div class="row" style="padding-top:10px;">
+              <div class="center-align">
+                <a class="btn-flat blue waves-effect waves-light lighten-1 white-text" href="/"> Home </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col s12 m8 offset-m2" style="margin-top:10px;">
+
+          @yield('content')
+              </div>
+          </div>
+      </div>
+    </body>
+    <script src="{{ URL::asset('jquery.min.js') }}"></script>
+    {!! MaterializeCSS::include_js() !!}
+    <script src="{{ URL::asset('init.js') }}"></script>
+
+</html>
